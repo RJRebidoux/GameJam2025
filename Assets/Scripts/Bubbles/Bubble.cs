a6170ff8a7f66263f0c6d6b5e8fd1c431cfe30e0
Matthew Brown <matthewb40000@gmail.com>
2025-01-25 11:31:34 -0500

    LUNCH YUM YUM

--- Assets/Scripts/Bubbles/Bubble.cs
+++ Assets/Scripts/Bubbles/Bubble.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Bubble : MonoBehaviour
 {
-    public float bounceForce = 5f;
+    public float bounceForce = 10f;
 
     // Start is called before the first frame update
     void Start()
