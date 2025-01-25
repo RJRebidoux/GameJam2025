2c9327968a034eeebf0afb0aa3dbc6467c0b495c
RJ Rebidoux <reeganrebidoux@hotmail.com>
2025-01-25 14:14:52 -0500

    Got Camera to move on a rail

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CameraController : MonoBehaviour
 {
     // References
+    public Transform[] waypoints; // List of points the camera moves between
+    public float speed = 2f; // Speed of the camera
 
+    private int currentIndex = 0;
+    public bool moving = false;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +21,8 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 targetPosition = waypoints[currentIndex].position;
+        Debug.Log("Target Position: " + targetPosition);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.fixedDeltaTime);
     }
 }
