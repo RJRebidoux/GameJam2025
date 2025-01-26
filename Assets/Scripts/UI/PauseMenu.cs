5128cd014e00693e24b1927c9d110e7c45b4cc89
nessatuncay <200573753@student.georgianc.on.ca>
2025-01-26 11:27:50 -0500

    Pause menu works better

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -6,17 +6,21 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
+    [SerializeField] GameObject camera;
 
     public void Pause()
     {
         pauseMenu.SetActive(true);
+        camera.SetActive(false);
         Time.timeScale = 0f;
+
 
     }
 
     public void Resume()
     {
         pauseMenu.SetActive(false);
+        camera.SetActive(true);
         Time.timeScale = 1f;
     }
 }
