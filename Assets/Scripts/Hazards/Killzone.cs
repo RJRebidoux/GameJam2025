4e7fbb812f538051da997da0dd2b9e27d9f55f03
RJ Rebidoux <reeganrebidoux@hotmail.com>
2025-01-25 23:18:57 -0500

    Killzone resets bubbles on repawn

--- Assets/Scripts/Hazards/Killzone.cs
+++ Assets/Scripts/Hazards/Killzone.cs
@@ -7,6 +7,8 @@ public class KillBox_Behaviour : MonoBehaviour
     // References
     public Transform respawn;
     public Transform cameralocation;
+    public GameObject[] bubbles;
+
     public void OnCollisionEnter2D(Collision2D collision)
     {
         // If the bullet collides with an Enemy, That Enemy takes damage;
@@ -14,6 +16,16 @@ public class KillBox_Behaviour : MonoBehaviour
         {
             collision.transform.position = respawn.position;
             cameralocation.position = new Vector3(respawn.position.x, respawn.position.y, cameralocation.position.z);
+
+            ActivateAllBubbles();
+        }
+    }
+
+    private void ActivateAllBubbles()
+    {
+        foreach (GameObject obj in bubbles)
+        {
+           obj.SetActive(true);  
         }
     }
 }
